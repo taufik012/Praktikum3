4c3381d64de678095f0cb5ed32a54cf3b1020c13
Taufk Hidayat <115480692+taufik012@users.noreply.github.com>
2022-11-01 09:35:29 +0700

    Add files via upload

---
+++ Latihan2.py
@@ -0,0 +1,13 @@
+a=input("masukan nilai a : ")
+b=input("masukan nilai b : ")
+print()
+print("variabel a =",a)
+print("variabel b =",b)
+print()
+print("hasil penggabungan {1}&{0} = %s".format(a,b) %(a+b))
+
+#konversi nilai variabel
+a=int(a)
+b=int(b)
+print("hasil penjumlahan  {1}+{0} = %s".format(a,b) %(a+b))
+print("hasil pembagian    {1}/{0} = %s".format(a,b) %(a/b))
---
+++ Latihan3.py
@@ -0,0 +1,12 @@
+print('Hello ini script python')
+print()
+
+print('{0:>16}'.format('*'))
+print('{0:>17}'.format('***'))
+print('{0:>18}'.format('*****'))
+print('{0:>19}'.format('*******'))
+print('{0:>20}'.format('*********'))
+print('{0:>19}'.format('*******'))
+print('{0:>18}'.format('*****'))
+print('{0:>17}'.format('***'))
+print('{0:>16}'.format('*'))
